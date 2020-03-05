a7455fda1e10bec289f02eb96f385204cd4813d7
Adam Gajzlerowicz <nelf86@gmail.com>
2020-03-05 13:04:01 +0100

    Added prettier config to package.json

--- tests/lib/rules/no-parent-imports.test.js
+++ tests/lib/rules/no-parent-imports.test.js
@@ -1,39 +1,39 @@
-const RuleTester = require("eslint").RuleTester;
+const RuleTester = require('eslint').RuleTester
 
-const { noParentImports } = require("../../../lib/rules/no-parent-imports");
+const { noParentImports } = require('../../../lib/rules/no-parent-imports')
 
 RuleTester.setDefaultConfig({
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: "module"
-  }
-});
-
-const ruleTester = new RuleTester();
-
-ruleTester.run("no-parent-imports", noParentImports, {
-  valid: [
-    'import React from "react";',
-    'import foo from "../foo";',
-    'import foo from "foo";',
-    'import foo from "foo/foo";'
-  ],
-  invalid: [
-    {
-      code: 'import foo from "../../../foo";',
-      errors: [
-        {
-          message: noParentImports.message
-        }
-      ]
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
     },
-    {
-      code: 'import foo from "../../foo";',
-      errors: [
+})
+
+const ruleTester = new RuleTester()
+
+ruleTester.run('no-parent-imports', noParentImports, {
+    valid: [
+        'import React from "react";',
+        'import foo from "../foo";',
+        'import foo from "foo";',
+        'import foo from "foo/foo";',
+    ],
+    invalid: [
         {
-          message: noParentImports.message
-        }
-      ]
-    }
-  ]
-});
+            code: 'import foo from "../../../foo";',
+            errors: [
+                {
+                    message: noParentImports.message,
+                },
+            ],
+        },
+        {
+            code: 'import foo from "../../foo";',
+            errors: [
+                {
+                    message: noParentImports.message,
+                },
+            ],
+        },
+    ],
+})
