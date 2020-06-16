3a26d0c2e8932ebae6a13c7fff4cc03c5605056d
Adam Gajzlerowicz <nelf86@gmail.com>
2020-06-16 23:32:16 +0200

    Updated after checks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { noAbsoluteInternalImports } = require('./lib/rules/no-absolute-internal-imports')
 const { noParentImports } = require('./lib/rules/no-parent-imports')
 const { noPrivateImports } = require('./lib/rules/no-private-imports')
+const { noReactImport } = require('./lib/rules/no-react-import')
 const { typescript } = require('./lib/configs/typescript')
 const { flow } = require('./lib/configs/flow')
 const { recommended } = require('./lib/configs/recommended')
@@ -15,5 +16,6 @@ module.exports = {
         'no-absolute-internal-imports': noAbsoluteInternalImports,
         'no-parent-imports': noParentImports,
         'no-private-imports': noPrivateImports,
+        'no-react-import': noReactImport,
     },
 }
