9291b4c0b21d69813562df15552c7fe2dfdd74dc
Adam Gajzlerowicz <nelf86@gmail.com>
2020-06-17 08:44:17 +0200

    added more test cases

--- tests/lib/rules/no-react-import.test.js
+++ tests/lib/rules/no-react-import.test.js
@@ -12,10 +12,18 @@ RuleTester.setDefaultConfig({
 const ruleTester = new RuleTester()
 
 ruleTester.run('no-react-import', noReactImport, {
-    valid: ['import * as React from "react";'],
+    valid: ['import * as React from "react";', 'import foo from "bar";', 'import Hello from "../";'],
     invalid: [
         {
             code: 'import React from "react";',
+            errors: [
+                {
+                    message: noReactImport.message,
+                },
+            ],
+        },
+        {
+            code: 'import Something from "react";',
             errors: [
                 {
                     message: noReactImport.message,
